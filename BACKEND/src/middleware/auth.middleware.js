c2a065a62b88b392fe771f55a276e1468558d5e9
Mohammed Mubasheer <md.mubasheersep5@gmail.com>
2026-05-11 17:25:17 +0530

    Added auth.middleware.js

---
+++ BACKEND/src/middleware/auth.middleware.js
@@ -0,0 +1,40 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.models.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const authHeader = req.header("Authorization");
+
+    const token =
+      req.cookies?.accessToken ||
+      (authHeader && authHeader.startsWith("Bearer ")
+        ? authHeader.replace("Bearer ", "")
+        : null);
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    let decodedToken;
+    try {
+      decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (err) {
+      throw new ApiError(401, "Invalid or expired token");
+    }
+
+    const user = await User.findById(decodedToken?._id)
+      .select("-password -refreshToken");
+
+    if (!user) {
+      throw new ApiError(401, "User not found");
+    }
+
+    req.user = user;
+    next();
+
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
